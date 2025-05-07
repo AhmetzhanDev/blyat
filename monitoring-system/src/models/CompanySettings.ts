07e7d08749687c7e7438c293918b16291e35e1bc
Lev <shin.l02@mail.ru>
2025-05-07 20:09:03 +0500

    fix

--- monitoring-system/src/models/CompanySettings.ts
+++ monitoring-system/src/models/CompanySettings.ts
@@ -1,61 +1,73 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 // Основная схема настроек с массивом компаний
 const companySettingsSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
-    // unique: true
-  },
-  id: {
-    type: String,
-    required: false,
-    // unique: true
-  },
-  nameCompany: {
-    type: String,
-    required: false
-  },
-  managerResponse: {
-    type: Number,
-    required: false
-  },
-  telegramGroupId: {
-    type: Number,
-    required: false
-  },
-  telegramInviteLink: {
-    type: String,
-    required: false
-  },
-  phoneNumber: {
-    type: String,
-    required: false
-  },
-  whatsappAuthorized: {
-    type: Boolean,
-    default: false
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  isRunning: {
-    type: Boolean,
-    default: false
-  },
-  messanger: { // whatsapp | instagram
-    type: String,
-    default: 'whatsapp'
-  },
-  accessToken: {
-    type: String,
-    required: false
-  },
-  instagramUserId: {
-    type: String,
-    required: false
-  }
-});
+	userId: {
+		type: String,
+		required: true,
+		// unique: true
+	},
+	id: {
+		type: String,
+		required: false,
+		// unique: true
+	},
+	nameCompany: {
+		type: String,
+		required: false,
+	},
+	managerResponse: {
+		type: Number,
+		required: false,
+	},
+	working_hours_start: {
+		type: String,
+		required: false,
+	},
+	working_hours_end: {
+		type: String,
+		required: false,
+	},
+	telegramGroupId: {
+		type: Number,
+		required: false,
+	},
+	telegramInviteLink: {
+		type: String,
+		required: false,
+	},
+	phoneNumber: {
+		type: String,
+		required: false,
+	},
+	whatsappAuthorized: {
+		type: Boolean,
+		default: false,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	isRunning: {
+		type: Boolean,
+		default: false,
+	},
+	messanger: {
+		// whatsapp | instagram
+		type: String,
+		default: 'whatsapp',
+	},
+	accessToken: {
+		type: String,
+		required: false,
+	},
+	instagramUserId: {
+		type: String,
+		required: false,
+	},
+})
 
-export const CompanySettings = mongoose.model('CompanySettings', companySettingsSchema); 
+export const CompanySettings = mongoose.model(
+	'CompanySettings',
+	companySettingsSchema
+)
