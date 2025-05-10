4e1d72216c235a14cb849ad6b98fb53364ace0b8
Lev <shin.l02@mail.ru>
2025-05-10 13:36:19 +0500

    ff

--- monitoring-system/src/whatsapp/dailyReport.ts
+++ monitoring-system/src/whatsapp/dailyReport.ts
@@ -14,7 +14,7 @@ export const initDailyReportCron = (messageMonitor: MessageMonitor) => {
 	// Тестовый режим - запуск каждую минуту
 	const testCron = '*/1 * * * *'
 	// Реальный режим - запуск в 21:00 каждый день
-	const realCron = '0 3 * * *'
+	const realCron = '38 8 * * *'
 
 	// Используем реальный режим
 	const job = new CronJob(realCron, async () => {
