503998c3f400dd3f7a5b09982d93a193c492552f
bibasss <your-email@example.com>
2025-05-26 17:19:23 +0500

    ok

--- monitoring-system/src/instagram/instagramController.ts
+++ monitoring-system/src/instagram/instagramController.ts
@@ -19,7 +19,7 @@ export class InstagramController {
 			return res.status(401).json({ error: 'Unauthorized' });
 		}
 
-		const inst_url = `https://api.instagram.com/oauth/authorize?client_id=${process.env.IG_CLIENT_ID}&redirect_uri=https://api.salestrack.kz/api/instagram/callback&scope=business_basic,business_manage_messages&response_type=code&state=${userId}`
+		const inst_url = `https://api.instagram.com/oauth/authorize?client_id=${process.env.IG_CLIENT_ID}&redirect_uri=${process.env.IG_REDIRECT_URI}&scope=business_basic,business_manage_messages&response_type=code&state=${userId}`
 
 		res.send({ url: inst_url })
 		return { url: inst_url }
